f97922011d88b9f811575fff9f9c8a39c5e82e45
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-12 17:47:52 -0700

    Set up index.html

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static('public'));
+app.use((req, res, next) => {
+  console.log(`Incoming ${req.method} request to ${req.path}`);
+  next();
+});
 
-app.get('/', (req, res) => {
+app.get('/products', (req, res) => {
   res.status(200).send('Hi');
 });
 
