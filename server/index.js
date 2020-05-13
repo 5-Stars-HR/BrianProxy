19762d005cc1e4d7205980055d676a109563cf93
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-12 19:26:03 -0700

    Get gallery component to render with data

--- server/index.js
+++ server/index.js
@@ -17,7 +17,19 @@ app.use((req, res, next) => {
 app.get('/product/:id', (req, res) => {
   axios.get(`http://localhost:3002/product/${req.params.id}`)
     .then((response) => res.status(200).send(response))
-    .catch((error) => console.log(error));
+    .catch((error) => res.status(500).send(error));
+});
+
+app.get('/product/:id/find-store', (req, res) => {
+  axios.get(`http://localhost:3002/product/${req.params.id}/find-store/?q=${req.query.q}`)
+    .then((response) => res.status(200).send(response))
+    .catch((error) => res.status(500).send(error));
+});
+
+app.get('/api/images/:id', (req, res) => {
+  axios.get(`http://localhost:3001/api/images/${req.params.id}`)
+    .then((response) => res.status(200).send(response.data))
+    .catch((error) => res.status(500).send(error));
 });
 
 app.listen(PORT, () => `Server listening on port ${3000}`);
